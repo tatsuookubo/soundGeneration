c82298c98ad1570ffc50cc74dba68f55ee17d864
avbatchelor <alexbatchelor@hotmail.co.uk>
2015-12-02 13:14:19 -0500

    automatic stim playing changes

--- OneHundredPips.m
+++ OneHundredPips.m
@@ -1,4 +1,4 @@
-%%
+%% Pips 
 stim = PipStimulus; 
 stim.maxVoltage = 1; 
 stim.numPips = 60; 
@@ -7,11 +7,21 @@ for i = 1:100
     playSound(stim) 
 end
 
-%% 
+%% SineWave
 stim = SineWave; 
 stim.carrierFreqHz = 50; 
 stim.maxVoltage = 2; 
 plot(stim) 
+for i = 1:100 
+    playSound(stim) 
+end
+
+%% Chipr
+stim = Chirp;
+stim.maxVoltage = 1;
+stim.startPadDur = 5; 
+stim.endPadDur = 10;
+plot(stim)
 for i = 1:100 
     playSound(stim) 
 end
